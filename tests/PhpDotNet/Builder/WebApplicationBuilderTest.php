c9ae519fd8b4d510345c38abf176b20eea179974
JustinianCodes <justin.erdmier@outlook.com>
2022-05-25 01:04:25 -0500

    Create WebApplicationBuilderTest.php.

---
+++ tests/PhpDotNet/Builder/WebApplicationBuilderTest.php
@@ -0,0 +1,16 @@
+<?php
+
+/******************************************************************************
+ * Copyright (c) 2022. Justin Erdmier - All Rights Reserved                   *
+ * Licensed under the MIT License - See LICENSE in repository root.           *
+ ******************************************************************************/
+
+declare(strict_types = 1);
+
+namespace PhpDotNet\Builder;
+
+use PHPUnit\Framework\TestCase;
+
+class WebApplicationBuilderTest extends TestCase {
+
+}
